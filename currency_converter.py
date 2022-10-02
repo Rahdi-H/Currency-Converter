0fd2d77f03c44695aae6ee91c2f9a6dfa7eb8d70
Rahdi <rhrahdi@gmail.com>
2022-10-02 10:11:28 +0600

    init

---
+++ currency_converter.py
@@ -0,0 +1,7 @@
+def main():
+    print('currency converter Taka to Dollar')
+    inputt = eval(input('enter the amount of TK'))
+    dlr = inputt / 103
+    print(inputt, " taka is equal to ", dlr)
+
+main()
